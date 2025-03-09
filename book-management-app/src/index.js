c2338c9a07fd22f2400dfaa7966898eab11e332e
lucianobrsts <lucianobrsts@yahoo.com.br>
2025-03-09 10:50:22 -0300

    Resolvido problemas de start da aplicação

--- book-management-app/src/index.js
+++ book-management-app/src/index.js
@@ -3,5 +3,14 @@ import ReactDOM from 'react-dom';
 import AppRouter from './router/AppRouter';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.scss';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+    <StrictMode>
+      <AppRouter />
+    </StrictMode>,
+  );
